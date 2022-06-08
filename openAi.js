5a3d29a0585db08a458ecc9a4e9f8778f43c5daa
Scott Rohrig <65373836+scottrohrig@users.noreply.github.com>
2022-06-08 09:16:42 -0700

    validate null api key

--- openAi.js
+++ openAi.js
@@ -1,26 +1,38 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-  organization: "org-tKbSt4ZPkZkLU2DKLm6k3Lgm",
-  apiKey: process.env.OPENAI_API_KEY || ""
-});
+import { Configuration, OpenAIApi } from 'openai';
 
+const configuration = new Configuration({
+  organization: 'org-tKbSt4ZPkZkLU2DKLm6k3Lgm',
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
 const openai = new OpenAIApi(configuration);
 
+const completionOptions = {
+  prompt,
+  temperature: 0.7,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+};
+
 const getEngines = async () => {
-  const response = await openai.listEngines();
-  return response.data;
+  try {
+    const response = await openai.listEngines();
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { data: [{ id: 'add your OPENAI_API_KEY' }] };
+  }
 };
 
 const postPrompt = async (model, prompt) => {
-  const response = await openai.createCompletion(model, {
-    prompt,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0
-  });
+  if (!configuration.apiKey) {
+    return {
+      choices: [{ text: 'You must add your OpenAi API Key to openAi.js' }],
+    };
+  }
+  const response = await openai.createCompletion(model, completionOptions);
   return response.data;
 };
 
-export { getEngines, postPrompt };
+export { getEngines, postPrompt, completionOptions };
